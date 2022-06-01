b94e8d264cb55b9a30800f0b960d0f451a9336cc
yahyawii <assadyahyawi501@gmail.com>
2022-06-01 15:10:34 +0200

    first commit

---
+++ src/components/CompoNAV.js
@@ -0,0 +1,31 @@
+import { Nav } from 'react-bootstrap';
+
+let CompoNAV=()=>{
+
+return(
+    <div>
+<Nav
+  activeKey="/home"
+  onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+>
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
+    </div>
+)
+
+}
+
+export default CompoNAV
---
+++ src/components/Example.js
@@ -0,0 +1,10 @@
+const Excample =()=> {
+    var msg="hello man"
+    var msg2="goodbye man"
+    return (
+        <div>
+        <h1>{msg}</h1>
+        <h2> {msg2} </h2>
+        </div>
+    )
+}
